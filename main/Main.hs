ccffff2091b5f9b332de4c9fe3fe33623be9ff93
Matthew Pickering <matthewtpickering@gmail.com>
2018-12-22 14:04:02 +0000

    Link together ticket list and issue page

--- main/Main.hs
+++ main/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 import GitLab.Tickets
@@ -44,7 +45,10 @@ import Brick.Widgets.Core
 import Brick.Util (fg, on)
 
 import Data.Generics.Product
-import GHC.Generics
+import GHC.Generics (Generic)
+
+import Control.Monad.IO.Class (liftIO)
+
 
 view = flip (^.)
 
@@ -69,40 +73,89 @@ main = do
   let env = mkClientEnv mgr gitlabApiBaseUrl
   es_raw <- runClientM (getIssue token project) env
   let es = either (error . show) id es_raw
-  es_n_raw <- runClientM (listIssueNotes token Nothing project ( irIid (es !! 0))) env
-  let es_n = either (error . show) id es_n_raw
-  gui (initialState env token es es_n)
+  gui (initialState env token es)
 
 
 
 
 drawUI :: AppState -> [Widget Name]
-drawUI l = [ui']
-    where
-        label = str "Item " <+> cur <+> str " of " <+> total
-        cur = case (issues l) ^. (L.listSelectedL) of
-                Nothing -> str "-"
-                Just i -> str (show (i + 1))
-        total = str $ show $ Vec.length $ (issues l) ^. (L.listElementsL)
-        box = B.borderWithLabel label $
-              L.renderList listDrawElement True (issues l)
-        ui = C.vCenter $ vBox [ C.hCenter box
-                              ]
+drawUI l =
+    case view (field @"mode") l of
+      TicketList -> ticketListWidget l
+      IssueView  -> issueViewWidget (view typed l)
 
-        ui' = issuePage (L.listElements (issues l) Vec.! 0) (issueNotes l)
+ticketListWidget :: AppState -> [Widget Name]
+ticketListWidget l = [ui]
+  where
+    label = str "Item " <+> cur <+> str " of " <+> total
+    cur = case (issues l) ^. (L.listSelectedL) of
+              Nothing -> str "-"
+              Just i -> str (show (i + 1))
+    total = str $ show $ Vec.length $ (issues l) ^. (L.listElementsL)
+    box = B.borderWithLabel label $
+            L.renderList listDrawElement True (issues l)
+    ui = C.vCenter $ vBox [ C.hCenter box
+                          ]
+
+errorPage :: Widget n -> Widget n
+errorPage reason = str "Error: " <+> reason
+
+issueViewWidget :: Maybe IssuePage -> [Widget Name]
+issueViewWidget l =
+  case l of
+    Nothing -> [errorPage (str "IssuePage was empty")]
+    Just p  -> [issuePage p]
 
 appEvent :: AppState -> T.BrickEvent Name e -> T.EventM Name (T.Next AppState)
 appEvent l (T.VtyEvent e) =
     case e of
         V.EvKey V.KEsc [] -> M.halt l
 
+        V.EvKey V.KEnter [] ->
+          case view (field @"mode") l of
+            TicketList -> ticketListEnter l
+            IssueView  -> M.continue l
+
         ev -> do
           i' <- L.handleListEvent ev (view (field @"issues") l)
-          in' <- L.handleListEvent ev (view (field @"issueNotes") l)
-          M.continue (set (field @"issues") i'
-                     . set (field @"issueNotes") in'
-                     $ l)
+          (l' :: AppState)
+              <- case (view (field @"issuePageState") l) of
+                   Nothing -> return l
+                   Just xs -> do
+                    in' <- L.handleListEvent ev (view (field @"issueNotes") xs)
+                    return $ set (field @"issuePageState"
+                                 . mapped
+                                 . field @"issueNotes")
+                                 in' l
+
+          M.continue (set (field @"issues") i' l')
 appEvent l _ = M.continue l
+
+ticketListEnter :: AppState -> T.EventM Name (T.Next AppState)
+ticketListEnter l = do
+  let cursorTicket = view (field @"issues" . to L.listSelectedElement) l
+  case cursorTicket of
+    Nothing -> M.continue l
+    Just (_, t) ->
+      liftIO (loadTicket t l) >>=
+            M.continue . set typed IssueView
+
+setMode :: Mode -> AppState -> AppState
+setMode = set typed
+
+loadTicket :: IssueResp -> AppState -> IO AppState
+loadTicket t l = do
+  let iid = view (field @"irIid") t
+      token   = view (field @"token") l
+      reqEnv  = view (field @"reqEnv") l
+  es_n_raw <-
+    runClientM (listIssueNotes token Nothing project iid) reqEnv
+  let es_n = either (error . show) id es_n_raw
+  return $
+    set typed
+        (Just (IssuePage (L.list Notes (Vec.fromList es_n) 1) t))
+        l
+
 
 listDrawElement :: Bool -> IssueResp -> Widget n
 listDrawElement sel IssueResp{..} =
@@ -114,10 +167,13 @@ listDrawElement sel IssueResp{..} =
         , hLimit 50 $ padRight Max $ txt irTitle
         , padLeft (Pad 1) $ txt irState]
 
-issuePage :: IssueResp -> L.List Name IssueNoteResp -> Widget Name
-issuePage IssueResp{..} notes =
+issuePage :: IssuePage -> Widget Name
+issuePage l =
   joinBorders $ B.border (vBox [metainfo, desc, notesSect, B.hBorder, footer])
   where
+    IssueResp{..} = view typed l
+    notes    = view typed l
+
     boxLabel = int (unIssueIid irIid)
 
     titleBox = boxLabel
@@ -168,11 +224,12 @@ metaRow label widget = vLimit 2 (B.hBorderWithLabel (txt label))
 
 
 initialState :: ClientEnv -> AccessToken -> [IssueResp]
-             -> [IssueNoteResp] -> AppState
-initialState env token es es_n =
+             -> AppState
+initialState env token es =
   AppState {
-      issues = L.list IssueList (Vec.fromList es) 1
-    , issueNotes = L.list Notes (Vec.fromList es_n) 1
+      mode = TicketList
+    , issues = L.list IssueList (Vec.fromList es) 1
+    , issuePageState = Nothing
     , reqEnv = env
     , token  = token }
 
@@ -186,8 +243,18 @@ theMap = A.attrMap V.defAttr
     , (customAttr,            fg V.cyan)
     ]
 
-data AppState = AppState { issues :: L.List Name IssueResp
-                         , issueNotes :: L.List Name IssueNoteResp
+data IssuePage = IssuePage {
+                  issueNotes :: L.List Name IssueNoteResp,
+                  currentIssue :: IssueResp
+                  } deriving Generic
+
+data Mode = TicketList | IssueView
+
+
+data AppState = AppState { mode :: Mode
+                         , issues :: L.List Name IssueResp
+                         -- Populated on demand
+                         , issuePageState :: Maybe IssuePage
                          , reqEnv :: ClientEnv
                          , token  :: AccessToken } deriving Generic
 
